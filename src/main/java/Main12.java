234e02f46bc0cc81e0223bce9388f0e94ee9cf9d
Pau Segovia <psegovia@cifpfbmoll.eu>
2021-04-19 18:33:31 +0200

    Último ej

--- src/main/java/Main12.java
+++ src/main/java/Main12.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main12 {
     public static void main(String[] args) {
-        
+        Scanner numero = new Scanner(System.in);
+        System.out.println("Introduce un número entero positivo");
+        String longitud= numero.nextLine();
+        System.out.println("El número introducido tiene "+longitud.length()+" cifras");
+        }
     }
-}
